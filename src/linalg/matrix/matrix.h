4206b5c3783da7a5eafcd781565e158cc8c150df
recabet <rajab.iskenderli@gmail.com>
2025-08-10 17:06:58 +0400

    created inplace row and column operations

--- src/linalg/matrix/matrix.h
+++ src/linalg/matrix/matrix.h
@@ -2,8 +2,9 @@
 // Created by recabet on 8/3/25.
 //
 
-#ifndef LINALG_MATRIX_H
-#define LINALG_MATRIX_H
+#ifndef CML_LINALG_MATRIX_H
+#define CML_LINALG_MATRIX_H
+
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -21,7 +22,7 @@ typedef struct matrix matrix;
 
 matrix* matrix_init(uint32_t rows, uint32_t cols, bool randomize);
 
-float32_t matrix_get(const matrix* m,uint32_t row, uint32_t col);
+float32_t matrix_get(const matrix* m, uint32_t row, uint32_t col);
 
 void matrix_set(const matrix* m, uint32_t row, uint32_t col, float32_t value);
 
@@ -45,6 +46,8 @@ matrix* matrix_copy(const matrix* m);
 
 matrix* matrix_scalar_mul(const matrix* m, float32_t scalar);
 
+void matrix_scalar_mul_(const matrix* m, float32_t scalar);
+
 matrix* matrix_transpose(const matrix* m);
 
 void matrix_transpose_(const matrix* m);
@@ -53,6 +56,16 @@ void matrix_transpose_(const matrix* m);
 
 float32_t matrix_trace(const matrix* m);
 
+void matrix_row_swap_(const matrix* m, uint32_t r1, uint32_t r2);
 
+void matrix_row_scale_(const matrix* m, uint32_t r, float32_t scalar);
 
-#endif //LINALG_MATRIX_H
+void matrix_row_add_multiple_(const matrix* m, uint32_t src, uint32_t dst, float32_t scalar);
+
+void matrix_col_swap_(const matrix* m, uint32_t c1, uint32_t c2);
+
+void matrix_col_scale_(const matrix* m, uint32_t c, float32_t scalar);
+
+void matrix_col_add_multiple_(const matrix* m, uint32_t src, uint32_t dst, float32_t scalar);
+
+#endif //CML_LINALG_MATRIX_H
