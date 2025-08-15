0ca616d607f561a6616e3c0f6c940d871c799d0d
Suleyman Farajli <suleyman@farajli.net>
2025-08-15 22:45:28 +0400

    feat: tensor api updated

--- src/tensor.h
+++ src/tensor.h
@@ -1,6 +1,23 @@
+#ifndef __TENSOR_H__
+#define __TENSOR_H__
+
 typedef struct {
     float *data;
     int *shape;
     int *strides;
     int ndim;
 } Tensor;
+
+/* Creation */
+Tensor* tensor_init(int ndim, const int *shape);
+Tensor* tensor_zeros(int ndim, const int *shape);
+Tensor* tensor_ones(int ndim, const int *shape);
+
+/* Memory Management*/
+void tensor_free(Tensor *t);
+
+/* Operations */
+void tensor_copy(Tensor *t);
+void tensor_fill(Tensor *t, float value);
+
+#endif
